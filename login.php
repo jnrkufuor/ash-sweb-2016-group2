5a2b34ce353a30fc1c63b732ff49a468dff572dd
Jay Kufuor <Jay Kufuor>
2016-03-18 14:51:29 +0000

    Updated

--- login.php
+++ login.php
@@ -11,6 +11,12 @@
             $id =$_REQUEST['id'];
             header("Location:userlogin.php?id=$id&password=$password");
         }
+         else if(isset($_REQUEST['add']))
+        {
+            $password=$_REQUEST['password'];
+            $id =$_REQUEST['id'];
+            header("Location:usersadd.php");
+        }
         
         ?>
         <form>
@@ -18,16 +24,16 @@
             <fieldset class="account-info">
                 <label>
                     ID
-                    <input type="text" name="id">
+                    <input type="text" name="id" required="Required">
                 </label>
                 <label>
                     Password
-                    <input type="password" name="password">
+                    <input type="password" name="password" required="Required">
                 </label>
             </fieldset>
             <fieldset class="account-action">
                 <input class="btn" type="submit" name="submit" value="Login">
-           
+                <input class="btn" type="submit" name="add" value="Sign Up">
             </fieldset>
         </form>
        
