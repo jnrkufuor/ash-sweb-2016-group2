7dec9a93eb1967b37efc92f57366b7b1496dd0dc
Anna Addei <anna.addei@gmail.com>
2016-04-22 15:10:31 +0000

    commiting ajax for submission

--- Submission/risk.php
+++ Submission/risk.php
@@ -1,6 +1,76 @@
 <html>
 <head>
 	<link href="../UI template/index.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="js/jquery-1.12.1.js"></script>
+	<script type="text/javascript">
+
+		function back(id){
+			window.open("subjects.php?id="+id,"_self");
+		}
+
+		function next(id){
+			var procedureRisks = "";
+			if(document.getElementById('deception').checked){
+				procedureRisks = procedureRisks + "deception,";
+			}
+			if(document.getElementById('punishment').checked){
+			procedureRisks = procedureRisks + "punishment,";
+			}
+			if(document.getElementById('unacceptableMaterial').checked){
+			procedureRisks = procedureRisks + "unacceptableMaterial,";
+			}
+			if(document.getElementById('privacyInvasion').checked){
+			procedureRisks = procedureRisks + "privacyInvasion,";
+			}
+			if(document.getElementById('participantDisclosure').checked){
+			procedureRisks = procedureRisks + "participantDisclosure,";
+			}
+			if(document.getElementById('physicalInvasion').checked){
+			procedureRisks = procedureRisks + "physicalInvasion,";
+			}
+		
+			window.open("update.php?cmd=3&id="+id +"&procedureRisks="+ procedureRisks +"&procedureDetails=" + $("#procedureDetails").val() ,"_self");
+		}
+
+		function save(id){
+			var procedureRisks = "";
+
+			if(document.getElementById('deception').checked){
+				procedureRisks = procedureRisks + "deception,";
+			}
+			if(document.getElementById('punishment').checked){
+			procedureRisks = procedureRisks + "punishment,";
+			}
+			if(document.getElementById('unacceptableMaterial').checked){
+			procedureRisks = procedureRisks + "unacceptableMaterial,";
+			}
+			if(document.getElementById('privacyInvasion').checked){
+			procedureRisks = procedureRisks + "privacyInvasion,";
+			}
+			if(document.getElementById('participantDisclosure').checked){
+			procedureRisks = procedureRisks + "participantDisclosure,";
+			}
+			if(document.getElementById('physicalInvasion').checked){
+			procedureRisks = procedureRisks + "physicalInvasion,";
+			}
+
+			var theUrl="submission_ajax.php?cmd=4 & id="+id +"&procedureRisks="+procedureRisks +"&procedureDetails=" + $("#procedureDetails").val();
+				$.ajax(theUrl,
+					{async:true,complete:saveComplete}
+					);
+
+		}
+
+		function saveComplete(xhr,status){
+				if(status!="success"){
+					divStatus.innerHTML="error sending request";
+					return;
+				}
+				divStatus.innerHTML= "Saved at " + new Date().getHours()+ ":" + new Date().getMinutes() + " GMT" ;
+				alert("Saved at " + new Date().getHours()+ ":" + new Date().getMinutes() + " GMT");
+			}
+
+	</script>
 </head>
 <body>
 	<div class="main">
@@ -10,36 +80,82 @@
 		<a href="../UI template/deletefile.html" style ="text-decoration:none"><div id="filesys"><h3>File System</h3></div></a>
 		<a href="../UI template/review2.html" style ="text-decoration:none"><div id="rev"><h3>IRB Reviews</h3></div></a>
 	</div>
-	<!-- <div class="side2" >Side bar</div>-->
+
 	<div class="menu">
-		<span>Application Form</span>
+		<span>View Submissions</span>
 	</div>
 	<br><br>
 		<?php
+		include_once("submission.php");
+		$obj = new submission();
+
 		$id ="";
 
 		if(isset($_REQUEST['id'])){
 			$id = $_REQUEST['id'];
 		}
+
+		$r = $obj -> getSubmissionByCode($id);
+									
+		if(!$r){
+			echo "Error getting the submission";
+			exit();
+		}
+		else{
+			$row = $obj ->fetch();
+		}
+
+		$procedureRisks = explode(',', $row['procedureRisks']); 
+
+		$deception = "";
+		$punishment = "";
+		$unacceptableMaterial = "";
+		$participantDisclosure = "";
+		$privacyInvasion = "";
+		$physicalInvasion = "";
+
+		foreach ($procedureRisks as $value) {
+			if($value == "deception"){
+				$deception = "checked";
+			}
+			else if($value == "punishment"){
+				$punishment = "checked";
+			}
+			else if($value == "unacceptableMaterial"){
+				$unacceptableMaterial = "checked";
+			}
+			else if($value == "privacyInvasion"){
+				$privacyInvasion = "checked";
+			}
+			else if($value == "participantDisclosure"){
+				$participantDisclosure = "checked";
+			}
+			else if($value == "physicalInvasion"){
+				$physicalInvasion = "checked";
+			}
+
+		}
+
 		?>
-		<form style="margin-left:30%"action="updateRisk.php?" method="GET">
-		<div style="height:509px" class="mainDiv">
+		<form style="margin-left:28%"action="updateRisk.php?" method="GET">
+		<div style="height:559px" class="mainDiv">
 			<div><input type="hidden" name="id" value="<?php echo $id?>"/></div>
+			<div class="status" id="divStatus"></div>
 			<h2 id="headings">Risks Involved In The Research</h2>
 			<div><p>Identify potential risks for subjects to be involved in this project/research. What procedures will be in place to minimize any risks to the subjects?</p></div>
 			<div><b>Does the research involve any of the following procedures?</b></div>
-			<div><input type="checkbox" value="deception" name="procedureRisks[]">Deception of the participant?</div>
-			<div><input type="checkbox" value="punishment" name="procedureRisks[]">Punishment of the participant?</div>
-			<div><input type="checkbox" value="unacceptableMaterial" name="procedureRisks[]">Materials commonly regarded as socially unacceptable such as pornography, inflammatory text, ethnic portrayals?</div>
-			<div><input type="checkbox" value="privacyInvasion" name="procedureRisks[]">Any other procedure that might be considered an invasion of privacy?</div>
-			<div><input type="checkbox" value="participantDisclosure" name="procedureRisks[]">Disclosure of the names of individual participants?</div>
-			<div><input type="checkbox" value="physicalInvasion" name="procedureRisks[]">Any other physically invasive procedure?</div>
+			<div><input type="checkbox" id="deception" value="deception" name="procedureRisks" <?php echo $deception ?> >Deception of the participant?</div>
+			<div><input type="checkbox" id="punishment" value="punishment" name="procedureRisks" <?php echo $punishment ?> >Punishment of the participant?</div>
+			<div><input type="checkbox" id="unacceptableMaterial" value="unacceptableMaterial" name="procedureRisks" <?php echo $unacceptableMaterial ?> >Materials commonly regarded as socially unacceptable such as pornography, inflammatory text, ethnic portrayals?</div>
+			<div><input type="checkbox" id="privacyInvasion" value="privacyInvasion" name="procedureRisks" <?php echo $privacyInvasion ?> >Any other procedure that might be considered an invasion of privacy?</div>
+			<div><input type="checkbox" id="participantDisclosure" value="participantDisclosure" name="procedureRisks" <?php  echo$participantDisclosure ?> >Disclosure of the names of individual participants?</div>
+			<div><input type="checkbox" id="physicalInvasion" value="physicalInvasion" name="procedureRisks" <?php echo $physicalInvasion ?> >Any other physically invasive procedure?</div>
 			<br/>
 			<div><p>If the answer to any of the above is "Yes", please explain this procedure in detail and describe procedures for protecting against or minimizing any potential risk.</p></div>
-			<div><textarea name="procedureDetails" style="width:97%; height: 8%"></textarea></div>
+			<div><textarea id="procedureDetails" name="procedureDetails" style="width:97%; height: 8%"><?php echo $row['procedureDetails'] ?></textarea></div>
 			<br/>
 			<br/>
-			<div>Progress:<progress value="60" max="100"></progress> &nbsp; &nbsp; &nbsp; &nbsp; <input style="float:right;" type="submit" value="Next"> <a style="float:right; margin-right:40px;" href="subjects.php?id=<?php echo $id ?>">Back</a> &nbsp;</div>
+			<div>Progress:<progress value="60" max="100"></progress> &nbsp; &nbsp; &nbsp; &nbsp; <button style="float:right; margin-right: 20px" type="button" onclick="next(<?php echo $id ?>)" >Next</button> &nbsp; &nbsp;<button type="button" style="float:right; margin-right: 40px" onclick="save(<?php echo $id ?>)">Save</button> <button type="button" style="float:right; margin-right: 40px" onclick="back(<?php echo $id ?>)">Back</button> &nbsp;</div>
 		</form>
 		</div><br><br>
 	<footer><p>Ashesi University College. | All rights reserved. | University Avenue, Berekuso; PMB CT 3, Cantonments, Accra, Ghana | Phone: +233.302.610.330</p>
--- Submission/subjects.php
+++ Submission/subjects.php
@@ -1,6 +1,115 @@
 <html>
 <head>
 	<link href="../UI template/index.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="js/jquery-1.12.1.js"></script>
+	<script type="text/javascript">
+
+		function back(id){
+			window.open("exemptionRequest2.php?id="+id,"_self");
+		}
+
+		function next(id){
+			if($("#subjectCharacteristics").val() == ""){
+				document.getElementById("subjectCharacteristics").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("subjectCharacteristics").style.border="1px solid grey";
+			}
+			if($("#recruitment").val() == ""){
+				document.getElementById("recruitment").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("recruitment").style.border="1px solid grey";
+			}
+			if($("#partcipnatInfo").val() == ""){
+				document.getElementById("partcipnatInfo").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("partcipnatInfo").style.border="1px solid grey";
+			}
+			if($("#researchMethod").val() == ""){
+				document.getElementById("researchMethod").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("researchMethod").style.border="1px solid grey";
+			}
+			if($("#dataSources").val() == ""){
+				document.getElementById("dataSources").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("dataSources").style.border="1px solid grey";
+			}
+				window.open("update.php?cmd=2&id="+id +"&subjectCharacteristics="+$("#subjectCharacteristics").val()+"&specialClasses=" + $("#specialClasses").val()+"&recruitment="+$("#recruitment").val() +"&partcipnatInfo="+$("#partcipnatInfo").val()+"&researchMethod="+$("#researchMethod").val()+"&dataSources="+$("#dataSources").val() ,"_self");
+			}
+		
+
+		function save(id){
+			if($("#subjectCharacteristics").val() == ""){
+				document.getElementById("subjectCharacteristics").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("subjectCharacteristics").style.border="1px solid grey";
+			}
+			if($("#recruitment").val() == ""){
+				document.getElementById("recruitment").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("recruitment").style.border="1px solid grey";
+			}
+			if($("#partcipnatInfo").val() == ""){
+				document.getElementById("partcipnatInfo").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("partcipnatInfo").style.border="1px solid grey";
+			}
+			if($("#researchMethod").val() == ""){
+				document.getElementById("researchMethod").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("researchMethod").style.border="1px solid grey";
+			}
+			if($("#dataSources").val() == ""){
+				document.getElementById("dataSources").style.border="1px solid red";
+				alert("Kindly fill all required fields");
+				return;
+			}
+			else{
+				document.getElementById("dataSources").style.border="1px solid grey";
+			}
+			var theUrl="submission_ajax.php?cmd=3 & id="+id +"&subjectCharacteristics="+$("#subjectCharacteristics").val()+"&specialClasses=" + $("#specialClasses").val()+"&recruitment="+$("#recruitment").val() +"&partcipnatInfo="+$("#partcipnatInfo").val()+"&researchMethod="+$("#researchMethod").val()+"&dataSources="+$("#dataSources").val();
+				$.ajax(theUrl,
+					{async:true,complete:saveComplete}
+					);
+			}
+
+		function saveComplete(xhr,status){
+				if(status!="success"){
+					divStatus.innerHTML="error sending request";
+					return;
+				}
+				divStatus.innerHTML= "Saved at " + new Date().getHours()+ ":" + new Date().getMinutes() + " GMT" ;
+				alert("Saved at " + new Date().getHours()+ ":" + new Date().getMinutes() + " GMT");
+			}
+
+	</script>
 </head>
 <body>
 	<div class="main">
@@ -49,24 +158,25 @@
 		
 	?>
 			
-		<form style="margin-left:30%"action="updateSubjects.php?" method="GET">
-		<div style="height:1190px" class="mainDiv">
+		<form style="margin-left:30%">
+		<div style="height:1290px" class="mainDiv">
 			<div><input type="hidden" name="id" value="<?php echo $id ?>"/></div>
+			<div class="status" id="divStatus"></div>
 			<h2>Numbers, Types and Recruitment of Subjects</h2>
-			<div><p>A. Identify the numbers and characteristics of subjects (eg. age ranges, sex, ethnic background, health status, disabilities , etc.) It is recommended to provide the breakdown based on your sampling strategy.</p></div>
-			<div><textarea name="subjectCharacteristics" style="width:97%; height: 8%" required><?php echo $row['subjectCharacteristics'] ?> </textarea></div>
+			<div><p>A. Identify the numbers and characteristics of subjects (eg. age ranges, sex, ethnic background, health status, disabilities , etc.) It is recommended to provide the breakdown based on your sampling strategy. <i style="color: red">*required</i></p></div>
+			<div><textarea id="subjectCharacteristics" name="subjectCharacteristics" style="width:97%; height: 8%"><?php echo $row['subjectCharacteristics'] ?></textarea></div>
 			<div><p>B. Special cases. If applicable, explain the rationale for the use of special cases or subjects such as pregnant women, children, prisoners, mentally impaired, institutionalized, or others who are likely to be particulary vulnerable</p></div>
-			<div><textarea name="specialClasses" style="width:97%; height: 8%"><?php echo $row['specialClasses'] ?></textarea></div>
-			<div><p>C. How are the individual participants to be recruited for this research? Is it clear to the subjects that participation is voluntary and that they may withraw at any time without any negative consequences?</p></div>
-			<div><textarea name="recruitment" style="width:97%; height: 8%" required><?php echo $row['recruitment'] ?></textarea></div>
-			<div><p>D. To what extent and how are participants to be informed of research procedures before their participation?</p></div>
-			<div><textarea name="partcipnatInfo" style="width:97%; height: 8%" required><?php echo $row['partcipnatInfo'] ?></textarea></div>
-			<div><p>E. How will you classify your research method? (experiment, observation, modelling etc.). Specify all methods you anticipate to use.</p></div>
-			<div><textarea name="researchMethod" style="width:97%; height: 8%" required><?php echo $row['researchMethod'] ?></textarea></div>
-			<div><p>F. Specify the data sources you will use for your reserach. (eg. questionnaire, audio recording human resource files, experiment data, etc.)</p></div>
-			<div><textarea name="dataSources" style="width:97%; height: 8%" required><?php echo $row['dataSources'] ?></textarea></div>
+			<div><textarea id="specialClasses" name="specialClasses" style="width:97%; height: 8%"><?php echo $row['specialClasses'] ?></textarea></div>
+			<div><p>C. How are the individual participants to be recruited for this research? Is it clear to the subjects that participation is voluntary and that they may withraw at any time without any negative consequences? <i style="color: red">*required</i></p></div>
+			<div><textarea id="recruitment" name="recruitment" style="width:97%; height: 8%" required><?php echo $row['recruitment'] ?></textarea></div>
+			<div><p>D. To what extent and how are participants to be informed of research procedures before their participation? <i style="color: red">*required</i></p></div>
+			<div><textarea id="partcipnatInfo" name="partcipnatInfo" style="width:97%; height: 8%" required><?php echo $row['partcipnatInfo'] ?></textarea></div>
+			<div><p>E. How will you classify your research method? (experiment, observation, modelling etc.). Specify all methods you anticipate to use. <i style="color: red">*required</i></p></div>
+			<div><textarea id="researchMethod" name="researchMethod" style="width:97%; height: 8%" required><?php echo $row['researchMethod'] ?></textarea></div>
+			<div><p>F. Specify the data sources you will use for your reserach. (eg. questionnaire, audio recording human resource files, experiment data, etc.) <i style="color: red">*required</i></p></div>
+			<div><textarea id="dataSources" name="dataSources" style="width:97%; height: 8%" required><?php echo $row['dataSources'] ?></textarea></div>
 			<br/>
-			<div style="margin-top: 20px"> Progress:</b> &nbsp;<progress value="40" max="100"></progress> <input style="float:right;" type="submit" value="Next" >  &nbsp; &nbsp; &nbsp; <a style="float:right; margin-right: 40px" href="exemptionRequest2.php?id=<?php echo $id ?>">Back</a> &nbsp; </div> 
+			<div style="margin-top: 20px"> Progress:</b> &nbsp;<progress value="40" max="100"></progress> <button style="float:right; margin-right: 20px" type="button" onclick="next(<?php echo $id ?>)" >Next</button> &nbsp; &nbsp;<button type="button" style="float:right; margin-right: 40px" onclick="save(<?php echo $id ?>)">Save</button> <button type="button" style="float:right; margin-right: 40px" onclick="back(<?php echo $id ?>)">Back</button> &nbsp; </div> 
 		</div>
 		</form><br><br>
 		</div>
