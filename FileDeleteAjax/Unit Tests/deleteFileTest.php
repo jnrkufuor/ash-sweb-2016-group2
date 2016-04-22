c9c9cd4910071898e85f2756338d22abbbe1813c
Dollawende Inusah <Dollawende Inusah>
2016-04-22 18:28:37 +0100

    delete file ajax implementation and documentation

---
+++ FileDeleteAjax/Unit Tests/deleteFileTest.php
@@ -0,0 +1,13 @@
+<?php
+include_once("../usersajax.php");
+
+class deleteFileTest extends PHPUnit_Framework_TestCase{
+
+	function testDeleteFile (){
+		$URL="usersajax.php?cmd=1&usercode=27302017&fileID=24";
+		$this->assertTrue('{"result":1,"message":"File deleted"}',$URL);
+	}
+
+}	
+?>
+
