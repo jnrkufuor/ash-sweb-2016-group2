876eb32699ae0304b3585211350501a433edd235
Anna Addei <anna.addei@gmail.com>
2016-04-23 17:16:26 +0000

    commiting fixes to regex on next button for benefits page

--- Implementation 2/confidentiality.php
+++ Implementation 2/confidentiality.php
@@ -50,8 +50,8 @@
 				document.getElementById("subjectInfo").style.border="1px solid grey";
 			}
 
-			if(validate($("#confidentialityExtent").val()) == false){
-				document.getElementById("confidentialityExtent").style.border="1px solid red";
+			if(validate($("#confidentialityProtection").val()) == false){
+				document.getElementById("confidentialityProtection").style.border="1px solid red";
 				alert("Kindly fill all required fields");
 				return;
 			}
