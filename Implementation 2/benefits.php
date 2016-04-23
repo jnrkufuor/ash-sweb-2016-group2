dfbecc48a11a3a73a1397462b767f4531136bf6f
Anna Addei <anna.addei@gmail.com>
2016-04-23 17:08:16 +0000

    commiting fixes to regex on next button for benefits page

--- Implementation 2/benefits.php
+++ Implementation 2/benefits.php
@@ -14,8 +14,8 @@
 		}
 		
 		function send(id){
-			if(validate($("#confidentialityExtent").val()) == false){
-				document.getElementById("confidentialityExtent").style.border="1px solid red";
+			if(validate($("#participantConpensation").val()) == false){
+				document.getElementById("participantConpensation").style.border="1px solid red";
 				alert("Kindly fill all required fields");
 				return;
 			}
@@ -23,8 +23,8 @@
 				document.getElementById("participantConpensation").style.border="1px solid grey";
 			}
 
-			if(validate($("#confidentialityExtent").val()) == false){
-				document.getElementById("confidentialityExtent").style.border="1px solid red";
+			if(validate($("#participantBenefits").val()) == false){
+				document.getElementById("participantBenefits").style.border="1px solid red";
 				alert("Kindly fill all required fields");
 				return;
 			}
@@ -37,8 +37,8 @@
 		
 
 		function save(id){
-			if($("#participantConpensation").val() == ""){
-				document.getElementById("participantConpensation").style.border="1px solid red";
+			if(validate($("#confidentialityExtent").val()) == false){
+				document.getElementById("confidentialityExtent").style.border="1px solid red";
 				alert("Kindly fill all required fields");
 				return;
 			}
@@ -46,8 +46,8 @@
 				document.getElementById("participantConpensation").style.border="1px solid grey";
 			}
 
-			if($("#participantBenefits").val() == ""){
-				document.getElementById("participantBenefits").style.border="1px solid red";
+			if(validate($("#confidentialityExtent").val()) == false){
+				document.getElementById("confidentialityExtent").style.border="1px solid red";
 				alert("Kindly fill all required fields");
 				return;
 			}
