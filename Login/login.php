d42a6992c2bc645839b8054b231b2d584522ade7
Jay Kufuor <Jay Kufuor>
2016-04-23 19:53:55 +0000

    Login Changes

--- Login/login.php
+++ Login/login.php
@@ -1,20 +1,27 @@
 <html>
     <head>
         <title>Login Page</title>
-        <link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="css/index.css">
-        <link rel="stylesheet" href="css/main.css">
-        <link href='http://fonts.googleapis.com/css?family=Average|Courgette' rel='stylesheet' type='text/css'>
-
-        <style>
-        body{
-            background-image: url("bg.jpg");
-        }
-        h1 {
-            font: 400 30px/0.5 'Courgette', Helvetica, sans-serif;
-            color:#395870;  
-        }
-        </style
+		<script type="text/javascript" src="js/jquery-1.12.1.js"></script>
+        <link hre type="text/javascript"f='http://fonts.googleapis.com/css?family=Average|Courgette' rel='stylesheet' type='text/css'>
+        <script type="text/javascript">
+		function login ()
+		{
+			validate($('#id').val());
+		}
+		
+		function validate(string)
+	    {
+	     var re = /([0-9]{8})/;
+		 
+	     if (re.test(string)==false)
+		 {
+			 alert("Please Use A Valid Ashesi ID(27302017)");
+			 document.getElementById("id").value="";
+		 }
+	    }
+	 	</script>
+      
     </head>
     <body>
 	     
@@ -24,12 +31,23 @@
 			  echo "<script type='text/javascript'> alert('Wrong Username or ID'); </script>";
 		 }
 		 ?>
-        <form action='userlogin.php' method='GET'>
+		 
+     <div class="div">
+     <div>
+      <fieldset class="section"> 
+       </fieldset>
+       <fieldset class="section2">
+           <h1 id="text" style="font-size: 270%;"> Welcome to the Ashesi IRB. </h1>
+
+    <br> </br><h2 id="text"> Sign In and Apply to Gain Permission to use Human Subjects In Your Research Work...</h2>
+       </fieldset>
+     </div>
+        <form action='userlogin.php' method='GET' id="form">
 
             <fieldset class="account-info">
                 <label>
                     ID
-                    <input type="text" name="id" required>
+                    <input type="text" id="id" name="id" onchange="login()" required>
                 </label>
                 <label>
                     Password
@@ -43,8 +61,8 @@
                 <a href="usersadd.php" >Join Now</a>
             </div>
             </fieldset>
-            
         </form>
+		</div>
 
     </body>
 </html>
--- Login/userajax.php
+++ Login/userajax.php
@@ -1,14 +1,52 @@
 <?php
-if isset($_REQUEST["cmd"])
+if (isset($_REQUEST["cmd"]))
 {
 	$cmd=$_REQUEST["cmd"];
 	switch ($cmd){
 		case 1: 
-		echo "";
+		deleteUsers();
 		break;
+		case 2:
+		deleteLec();
+		break;
+		default:
+		echo "";
 		
 	}
-	
 }
+	
+	function deleteUsers()
+	{
+		include("users.php");
+		$obj=new users();
+		if (isset($_REQUEST['id'])){
+		if(!$obj->deleteUser($_REQUEST['id']))
+		{
+		    echo '{"result":0,"message":"user not deleted"}';
+			return;
+		}
+
+			echo '{"result":1,"message":"user deleted"}';
+			return ;
+		}
+	
+	}
+    function deleteLec()
+	{
+		include("users.php");
+		$obj=new users();
+		if (isset($_REQUEST['id'])){
+		if(!$obj->deleteLec($_REQUEST['id']))
+		{
+		    echo '{"result":0,"message":"Reviewer not deleted"}';
+			return;
+		}
+
+			echo '{"result":1,"message":"Reviewer deleted"}';
+			return ;
+		}
+	
+	}
+
 
 ?>
