1db5e1ccfbb709a73b9c9750e5c565e5782f9f5d
Anna Addei <anna.addei@gmail.com>
2016-04-23 18:17:20 +0000

    commiting regex for exemption

--- Submission/exemptionRequest.php
+++ Submission/exemptionRequest.php
@@ -10,7 +10,7 @@
 		}
 
 		function checkSave(id){
-			if($("#title").val() == ""){
+			if(validate($("#title").val()) == false){
 				document.getElementById("title").style.border="1px solid red";
 				alert("Kindly provide a title for your submission");
 				return;
