eb0744a83a7b4b007326030958be9ff7340c22d9
Anna Addei <anna.addei@gmail.com>
2016-04-23 17:48:02 +0000

    commiting regex for subjects

--- Submission/subjects.php
+++ Submission/subjects.php
@@ -22,7 +22,7 @@
 			else{
 				document.getElementById("subjectCharacteristics").style.border="1px solid grey";
 			}
-			if($("#recruitment").val() == ""){
+			if(validate($("#recruitment").val()) == false){
 				document.getElementById("recruitment").style.border="1px solid red";
 				alert("Kindly fill all required fields");
 				return;
